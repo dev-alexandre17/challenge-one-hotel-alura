33a7d65d76d6bc6e713be4b0215107b0c69e1c34
dev-alexandre17 <alexandretec77@gmail.com>
2023-05-01 15:45:25 -0300

    Iniciando configurações básicas do spring boot

---
+++ backend/src/test/java/br/com/hotel/alura/api/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.hotel.alura.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
